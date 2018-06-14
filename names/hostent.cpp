bb183e3f0886b382d9c412b7fb4051921a4ad9e5
lihaifeng <1049400300@qq.com>
2018-06-15 00:39:54 +0800

    test gethostbyname in names/hostent.cpp
    1. also attention include_directories link_directories cmake_source_dir cmake_binary_dir

---
+++ names/hostent.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstdlib>
+#include <netdb.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include "lhf.h"
+////////////////////variables////////////////////
+////////////////////functions////////////////////
+////////////////////main////////////////////
+int main(int argc,char* argv[]){
+    for(int i=1;i<argc;i++){
+        char *name=argv[i];
+        struct hostent* phe; 
+        char** pstr;
+        if((phe = gethostbyname(name) )==NULL){
+            print_error(1,"gethostbyname",hstrerror(h_errno));
+        }
+        printf("h_name\t:%s\n",phe->h_name);
+        printf("h_aliases\t:\n");
+        for(pstr=phe->h_aliases;*pstr!=NULL;pstr++){
+            printf("\t %s\n",*pstr);
+        }
+        printf("h_length\t:%d\n",phe->h_length);
+        printf("h_addr_list\t:\n");
+        for(pstr=phe->h_addr_list;*pstr!=NULL;pstr++){
+            char buff[MAXLINE];
+            if(inet_ntop(AF_INET,*pstr,buff,sizeof(buff))==NULL){
+                print_error(1,"inet_ntop",strerror(errno));
+            }
+            printf("\t %s\n",buff);
+        }
+        printf("--------------------------------\n");
+    }
+    return 0;
+}
+
